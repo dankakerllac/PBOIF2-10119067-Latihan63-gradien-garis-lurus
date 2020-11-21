03f3b4fd81cab32b230c9c601e06883f71424f55
User <User@DESKTOP-6N6EHH6>
2020-11-21 23:13:18 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan63/gardien/garis/lurus/GarisLurus.java
@@ -0,0 +1,19 @@
+  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan63.gardien.garis.lurus;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Gradien Garis Lurus
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
